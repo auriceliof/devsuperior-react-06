0bbaeff7b00f09eba79e995adf4629d2c873fd31
auriceliof <auricelio.suporte@hotmail.com>
2022-08-24 10:21:51 -0300

    Component sales-by-date

---
+++ front-web/src/components/sales-by-date/index.tsx
@@ -0,0 +1,23 @@
+import './styles.css';
+
+function SalesByDate() {
+  return (
+    <div className="sales-by-date-container base-card">
+      <div className="sales-by-date-title">
+        <h4>Evolução de vendas</h4>
+        <span className="sales-by-date-period">01/01/2017 a 31/01/2017</span>
+      </div>
+      <div className="sales-by-date-data">
+        <div className="sales-by-date-quantity-container">
+          <h2 className="sales-by-date-quantity">464.988,00</h2>
+          <span className="sales-by-date-quantity-label">Vendas no período</span>
+          <span className="sales-by-date-quantity-description">
+            O gráfico mostra as vendas em todas as lojas
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SalesByDate;
